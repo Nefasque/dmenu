9fce45e257bc90a6fb143d0011a722d6bf412ed3
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-18 03:31:38 -0400

    patch vim-mode installed

--- config.def.h
+++ config.def.h
@@ -20,12 +20,15 @@ static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
 static char selbgcolor[]  = "#005577";
+static char cursorfgcolor[] = "#222222";
+static char cursorbgcolor[] = "#ffffff";
 
 static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { normfgcolor, normbgcolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
 	[SchemeOut]  = { "#000000",   "#00ffff" },
+  [SchemeCursor] = { cursorfgcolor, cursorbgcolor },
 };
 
 static unsigned int alphas[SchemeLast][2] = {
@@ -47,6 +50,18 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 0;
+
+/*
+ * -vi option; if nonzero, vi mode is always enabled and can be
+ * accessed with the global_esc keysym + mod mask
+ */
+static unsigned int vi_mode = 1;
+static unsigned int start_mode = 0;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
+static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
+static Key quit_keys[] = {
+	/* keysym	modifier */
+	{ XK_q,		0 }
+};
 
 
 /*
