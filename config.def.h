7ddfd1fc2ed25dce9b061d33722a9adc9afab7d0
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-18 02:17:33 -0400

    patch border installed

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xaa;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xfa;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -28,3 +28,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
