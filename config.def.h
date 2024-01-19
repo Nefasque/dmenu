8b0fe0e7bcc30cba35f162a0fec83a05334253fe
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-19 00:53:53 -0400

    patch fuzzy installed

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 static const unsigned int alpha = 0xfa;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
